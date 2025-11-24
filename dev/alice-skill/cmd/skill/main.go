e7ca9ae05fc2079046d5f763f861ccb1b3a74371
EVGENY ANTROPOV <burdaonelove@gmail.com>
2025-11-24 09:50:30 +0300

    feat: alice learnin 1-4 steps

---
+++ dev/alice-skill/cmd/skill/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Функция main вызывается автоматически при запуске приложения
+func main() {
+	// обрабатываем аргументы командной строки
+	parseFlags()
+
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+// Функция run будет полезна при инициализации зависимостей сервера перед запуском
+func run() error {
+	fmt.Println("Starting server on...", flagRunAddr)
+	return http.ListenAndServe(flagRunAddr, http.HandlerFunc(webhook))
+}
+
+// функция webhook — обработчик HTTP-запроса
+func webhook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		// разрешаем только POST-запросы
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	// установим правильный заголовок для типа данных
+	w.Header().Set("Content-Type", "application/json")
+	// пока установим ответ-заглушку, без проверки ошибок
+	_, _ = w.Write([]byte(`
+      {
+        "response": {
+          "text": "Извините, я пока ничего не умею"
+        },
+        "version": "1.0"
+      }
+    `))
+}
